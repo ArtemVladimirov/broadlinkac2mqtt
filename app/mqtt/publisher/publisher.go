4dbea7bbbc77bf68d4ccca690273bda2525f8f71
Artem Vladimirov <artem.vladimirov@agropoisk.ru>
2023-07-20 16:17:02 -0300

    bac-11 added context to mqtt publishers

--- app/mqtt/publisher/publisher.go
+++ app/mqtt/publisher/publisher.go
@@ -34,58 +34,94 @@ func (m *mqttPublisher) PublishDiscoveryTopic(ctx context.Context, logger *zerol
 	payload, err := json.Marshal(input.DiscoveryTopic)
 	if err != nil {
 		logger.Error().Err(err).Interface("input", input.DiscoveryTopic).Msg("Failed to marshal discovery topic")
+		return err
 	}
 
 	topic := *m.mqttConfig.AutoDiscoveryTopic + "/" + deviceClass + "/" + input.DiscoveryTopic.UniqueId + "/config"
-	m.client.Publish(topic, 0, m.mqttConfig.AutoDiscoveryTopicRetain, string(payload))
 
-	return nil
+	token := m.client.Publish(topic, 0, m.mqttConfig.AutoDiscoveryTopicRetain, string(payload))
+	select {
+	case <-ctx.Done():
+		return nil
+	case <-token.Done():
+		return nil
+	}
 }
 
 func (m *mqttPublisher) PublishAmbientTemp(ctx context.Context, logger *zerolog.Logger, input *models.PublishAmbientTempInput) error {
 
 	topic := m.mqttConfig.TopicPrefix + "/" + input.Mac + "/current_temp/value"
 
-	m.client.Publish(topic, 0, false, fmt.Sprintf("%.1f", input.Temperature))
-	return nil
+	token := m.client.Publish(topic, 0, false, fmt.Sprintf("%.1f", input.Temperature))
+	select {
+	case <-ctx.Done():
+		return nil
+	case <-token.Done():
+		return nil
+	}
 }
 
 func (m *mqttPublisher) PublishTemperature(ctx context.Context, logger *zerolog.Logger, input *models.PublishTemperatureInput) error {
 
 	topic := m.mqttConfig.TopicPrefix + "/" + input.Mac + "/temp/value"
 
-	m.client.Publish(topic, 0, false, fmt.Sprintf("%.1f", input.Temperature))
-	return nil
+	token := m.client.Publish(topic, 0, false, fmt.Sprintf("%.1f", input.Temperature))
+	select {
+	case <-ctx.Done():
+		return nil
+	case <-token.Done():
+		return nil
+	}
 }
 
 func (m *mqttPublisher) PublishMode(ctx context.Context, logger *zerolog.Logger, input *models.PublishModeInput) error {
 
 	topic := m.mqttConfig.TopicPrefix + "/" + input.Mac + "/mode/value"
 
-	m.client.Publish(topic, 0, false, input.Mode)
-	return nil
+	token := m.client.Publish(topic, 0, false, input.Mode)
+	select {
+	case <-ctx.Done():
+		return nil
+	case <-token.Done():
+		return nil
+	}
 }
 
 func (m *mqttPublisher) PublishSwingMode(ctx context.Context, logger *zerolog.Logger, input *models.PublishSwingModeInput) error {
 
 	topic := m.mqttConfig.TopicPrefix + "/" + input.Mac + "/swing_mode/value"
 
-	m.client.Publish(topic, 0, false, input.SwingMode)
-	return nil
+	token := m.client.Publish(topic, 0, false, input.SwingMode)
+	select {
+	case <-ctx.Done():
+		return nil
+	case <-token.Done():
+		return nil
+	}
 }
 
 func (m *mqttPublisher) PublishFanMode(ctx context.Context, logger *zerolog.Logger, input *models.PublishFanModeInput) error {
 
 	topic := m.mqttConfig.TopicPrefix + "/" + input.Mac + "/fan_mode/value"
 
-	m.client.Publish(topic, 0, false, input.FanMode)
-	return nil
+	token := m.client.Publish(topic, 0, false, input.FanMode)
+	select {
+	case <-ctx.Done():
+		return nil
+	case <-token.Done():
+		return nil
+	}
 }
 
 func (m *mqttPublisher) PublishAvailability(ctx context.Context, logger *zerolog.Logger, input *models.PublishAvailabilityInput) error {
 
 	topic := m.mqttConfig.TopicPrefix + "/" + input.Mac + "/availability/value"
 
-	m.client.Publish(topic, 0, false, input.Availability)
-	return nil
+	token := m.client.Publish(topic, 0, false, input.Availability)
+	select {
+	case <-ctx.Done():
+		return nil
+	case <-token.Done():
+		return nil
+	}
 }
